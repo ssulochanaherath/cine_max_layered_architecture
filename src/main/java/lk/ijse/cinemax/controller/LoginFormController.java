d99b8b2b54e06f43cd40a56817d07c3564b6c4f3
sangeeth <ssulochanaherath@gmail.com>
2023-11-08 03:46:02 +0530

    Signup form

--- src/main/java/lk/ijse/cinemax/controller/LoginFormController.java
+++ src/main/java/lk/ijse/cinemax/controller/LoginFormController.java
@@ -1,4 +1,11 @@
 package lk.ijse.cinemax.controller;
 
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public void btnLoginOnAction(ActionEvent event) {
+    }
+
+    public void btnSignUpOnAction(ActionEvent event) {
+    }
 }
