6ff37da089858cd708f4867d6cd68a940a9f48a2
sangeeth <ssulochanaherath@gmail.com>
2023-11-08 04:21:28 +0530

    Dashboard Form

--- src/main/java/lk/ijse/cinemax/controller/LoginFormController.java
+++ src/main/java/lk/ijse/cinemax/controller/LoginFormController.java
@@ -1,11 +1,32 @@
 package lk.ijse.cinemax.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginFormController {
+    public TextField txtId;
+    public TextField txtPassword;
+
     public void btnLoginOnAction(ActionEvent event) {
+
     }
 
-    public void btnSignUpOnAction(ActionEvent event) {
+    public void btnSignUpOnAction(ActionEvent event) throws Exception{
+        Node source = (Node) event.getSource();
+        Stage oldStage = (Stage) source.getScene().getWindow();
+
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/view/signup_form.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage newStage = new Stage();
+        newStage.setTitle("Signup Form");
+        newStage.setScene(scene);
+        newStage.show();
+
+        oldStage.close();
     }
 }
--- src/main/java/lk/ijse/cinemax/controller/SignupFormController.java
+++ src/main/java/lk/ijse/cinemax/controller/SignupFormController.java
@@ -1,4 +1,48 @@
 package lk.ijse.cinemax.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
 public class SignupFormController {
+
+    public TextField txtFristName;
+    public TextField txtLastName;
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void btnCreateAccountOnAction(ActionEvent event) throws Exception{
+        Node source = (Node) event.getSource();
+        Stage oldStage = (Stage) source.getScene().getWindow();
+
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/view/login_form.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage newStage = new Stage();
+        newStage.setTitle("Login Form");
+        newStage.setScene(scene);
+        newStage.show();
+
+        oldStage.close();
+    }
+
+    public void btnSignInOnAction(ActionEvent event) throws Exception{
+        Node source = (Node) event.getSource();
+        Stage oldStage = (Stage) source.getScene().getWindow();
+
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/view/login_form.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage newStage = new Stage();
+        newStage.setTitle("Login Form");
+        newStage.setScene(scene);
+        newStage.show();
+
+        oldStage.close();
+    }
 }
